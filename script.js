e0ef087e8a5b76ad625fa6d587485f1dfbbc2888
danikvsn <danikvasin@gmail.com>
2016-06-07 15:51:27 -0400

    Added Grid Format settings

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$('.dropdown-toggle').dropdown()
+
 function visibleSettings(visibility) {
     document.getElementById('settings-overlay').style.visibility = visibility;
     document.getElementById('settings-overlay-bg').style.visibility = visibility;
@@ -30,86 +32,66 @@ function displayTD() {
         date = "0" + date;
     }
     var month = currDate.getMonth();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    switch (month) {
-        case 0:
-            month = "January";
-            break;
-        case 1:
-            month = "February";
-            break;
-        case 2:
-            month = "March";
-            break;
-        case 3:
-            month = "April";
-            break;
-        case 4:
-            month  = "May";
-            break;
-        case 5:
-            month = "June";
-            break;
-        case 6:
-            month = "July";
-            break;
-        case 7:
-            month = "August";
-            break;
-        case 8:
-            month = "September";
-            break;
-        case 9:
-            month = "October";
-            break;
-        case 10:
-            month = "November";
-            break;
-        case 11:
-            month = "December";
-            break;
-    }
     var year = currDate.getFullYear();
     var dateDiv = document.getElementById('date');
-    dateDiv.innerText = date + " " + month + " " + year;
+    dateDiv.innerText = date + " " + months[month] + " " + year;
 
 }
 
-function setLayout(rows,cols) {
-
-
-    // Adds or Removes Rows
-    if (document.getElementsByClassName('link-container').children.length > rows){
-        while(document.getElementsByClassName('link-container').children.length > rows){
-            document.getElementsByClassName('link-container').removeChild(document.getElementsByClassName('link-container').lastChild)
+function setRow(rows) {
+    document.getElementById('row-drop').firstElementChild.innerHTML = rows;
+    if (document.getElementsByClassName('row').length > rows){
+        while(document.getElementsByClassName('row').length > rows){
+            document.getElementsByClassName('link-container')[0].removeChild(document.getElementsByClassName('link-container')[0].lastChild)
         }
-    }else{
-        while(document.getElementsByClassName('link-container').children.length < rows){
-
+    }else {
+        while (document.getElementsByClassName('row').length < rows) {
             var new_row = document.createElement('div');
             new_row.className = 'row';
-            document.getElementsByClassName('link-container').appendChild(new_row)
+            while (new_row.children.length < parseInt(document.getElementById('col-drop').firstElementChild.innerHTML)) {
+                var new_inner = document.createElement('div');
+                new_inner.className = 'inner';
+                new_inner.innerHTML = 'Test';
+                var new_col = document.createElement('div');
+                new_col.className = 'col-md-2 ';
+                new_col.className+='linkarea';
+                new_col.appendChild(new_inner);
+                new_row.appendChild(new_col);
+            }
+            document.getElementsByClassName('link-container')[0].appendChild(new_row)
         }
     }
+    console.log("New Rows: "+document.getElementsByClassName('row').length);
+}
+
+function setCol(cols){
+    document.getElementById('col-drop').firstElementChild.innerHTML = cols;
     // Adds or removes columns from existing rows
-    if (document.getElementsByClassName('row').children.length > cols) {
-        for(i = 0; i < document.getElementsByClassName('link-container').children.length;i++) {
-            while (document.getElementsByClassName('link-container').childNodes[i].children.length > cols) {
-                document.getElementsByClassName('link-container').childNodes[i].removeChild(document.getElementsByClassName('link-container').childNodes[i].lastChild)
+    console.log("Number of Cols: "+ document.getElementsByClassName('row')[0].children.length);
+    if (document.getElementsByClassName('row')[0].children.length > cols) {
+        console.log("Number of Rows: "+ document.getElementsByClassName('row').length);
+        for(i = 0; i < document.getElementsByClassName('row').length;i++) {
+
+            while (document.getElementsByClassName('row')[i].children.length > cols) {
+                console.log('removed col from '+i);
+                document.getElementsByClassName('row')[i].removeChild(document.getElementsByClassName('row')[i].lastChild)
             }
         }
     }else{
-        for(i = 0; i < document.getElementsByClassName('link-container').children.length;i++) {
-            while (document.getElementsByClassName('link-container').childNodes[i].children.length < cols) {
+        for(i = 0; i < document.getElementsByClassName('row').length;i++) {
+            while (document.getElementsByClassName('row')[i].children.length < cols) {
                 var new_inner = document.createElement('div');
                 new_inner.className = 'inner';
                 var new_col = document.createElement('div');
                 new_col.className = 'col-md-2';
                 new_col.appendChild(new_inner);
-                document.getElementsByClassName('link-container').childNodes[i].appendChild(new_col);
+                document.getElementsByClassName('row')[i].appendChild(new_col);
             }
         }
     }
-
+    console.log("New Cols: "+document.getElementsByClassName('row')[0].children.length);
 }
+
 
