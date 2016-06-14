d9c11147b49730d1a48816f46b198e8728b388a8
danikvsn <danikvasin@gmail.com>
2016-06-14 18:55:00 -0400

    Site now reads cookies or uses hardcoded defaults

--- script.js
+++ script.js
@@ -1,5 +1,96 @@
 $('.dropdown-toggle').dropdown()
 
+//Cookies Related Variables and Functions
+
+var numColumns = '';
+var numRows;
+var searchDefault;
+var arrLinks;
+
+var wlocation;
+var background_color;
+var background_image;
+var font_style;
+
+function getCookies() {
+    wlocation = getCookie('location');
+    searchDefault = getCookie('search');
+    if (searchDefault == null){
+        searchDefault = 'yandex';
+    }
+    font_style = getCookie('font');
+    if (font_style == null){
+        font_style = 'arial 16px white'
+    }
+    background_color = getCookie('background_color');
+    if (background_color == null){
+        background_color = '#222222'
+    }
+    background_image = getCookie('background_image');
+    numColumns = parseInt(getCookie('columns'));
+    if (getCookie('columns')== null){
+        numColumns = 4;
+    }
+    numRows = parseInt(getCookie('rows'));
+    if(getCookie('rows') == null){
+        numRows = 1;
+    }
+    arrLinks = getCookieLinks();
+    if(arrLinks == null){
+        arrLinks = [new Array(4)];
+        var links;
+        for(links in arrLinks[0]){
+            links = ['Links',[]];
+        }
+    }
+
+}
+
+function getCookie (cname){
+    var cookArray = document.cookie.split(';');
+    var cookie;
+    for (cookie in cookArray){
+        if (cookie.split('=')[0]==cname){
+            return cookie.split('=')[1];
+        }
+    }
+    return null;
+}
+
+function getCookieLinks() {
+    var linkCookie = getCookie('links');
+    if (linkCookie != null) {
+        var linkArray = linkCookie.replace('{', '').split('}');
+        console.log(linkArray);
+    }else{
+        return null;
+    }
+}
+
+//Functions
+window.onload = function() {
+    getCookies();
+    initLayout();
+    switch(location.hash){
+        case '#settings':
+            visibleSettings('visible');
+        break;
+    }
+
+}
+
+function initLayout(){
+    console.log("Search engine: " + searchDefault+"\nColumns: "+numColumns+"\nRows: "+numRows);
+    setSearchEngine(searchDefault);
+    setCol(numColumns);
+    setRow(numRows);
+
+}
+
+function setSearchEngine(engine){
+    //TODO: Implement Search Engine choosing
+}
+
 function visibleSettings(visibility) {
     document.getElementById('settings-overlay').style.visibility = visibility;
     document.getElementById('settings-overlay-bg').style.visibility = visibility;
@@ -84,8 +175,10 @@ function setCol(cols){
             while (document.getElementsByClassName('row')[i].children.length < cols) {
                 var new_inner = document.createElement('div');
                 new_inner.className = 'inner';
+                new_inner.innerHTML = 'Test';
                 var new_col = document.createElement('div');
-                new_col.className = 'col-md-2';
+                new_col.className = 'col-md-2 ';
+                new_col.className+='linkarea';
                 new_col.appendChild(new_inner);
                 document.getElementsByClassName('row')[i].appendChild(new_col);
             }
